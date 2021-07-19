ba769fd8abda5d5dc750166ca579a21550c55ff0
Felipe <felipe.horta@udarealestate.com>
2021-07-19 13:31:08 +0200

    update zyte report

--- functions/report_zyte_errors/main.py
+++ functions/report_zyte_errors/main.py
@@ -1,9 +1,15 @@
 from scrapinghub import ScrapinghubClient
 import pandas as pd
 
-from pprint import pprint
+import ssl
+from slack_sdk.webhook import WebhookClient
 
 apikey = 'b959a20caa184e45b176b47071e5c73a'
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+url = 'https://hooks.slack.com/services/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'
+webhook = WebhookClient(url)
 
 def main(context):
     client = ScrapinghubClient(apikey)
@@ -17,7 +23,38 @@ def main(context):
     df = pd.concat(dflist)
     df.fillna(0, inplace=True)
     df.eval('err_pct=errors/logs', inplace=True)
-    df.query('err_pct>.2', inplace=True)
+    df.query('err_pct>.1', inplace=True)
+
+    df.sort_values('err_pct', ascending=False, inplace=True)
+
+    msg = "Hi, don't worry about me! I'm a learning bot printing report ***bzzZZt*** messages.\n\n"
+    msg += "\nTake a look at what I've found about data-factory's latest crawlers errors: \n\n"
+
+
+    for spider, error, dt in zip(df.spider, df.err_pct, df.running_time):
+        error = round(error*100, 2)
+        #dt = pd.Timestamp(dt)
+        alert = ':fire:'*int(error/20)
+        msg += f'- {spider} {alert}: \n\t {error}%\n\n'
+
+    print(msg)
+
+    response = webhook.send(
+        text="execution report: ETAs",
+        blocks=[
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": msg
+                }
+            }
+        ]
+    )
+
+    assert response.status_code == 200
+    assert response.body == "ok"
+
     return df
 
 if __name__=='__main__':
